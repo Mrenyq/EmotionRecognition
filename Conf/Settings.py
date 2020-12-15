2b9f03bfb05b1e5ae071af7a52042e84d47e773d
westwing84 <nishihara-tsubasa-gn@ynu.jp>
2020-12-15 15:15:20 +0900

    Add Conf

---
+++ Conf/Settings.py
@@ -0,0 +1,45 @@
+# ECG Const
+FS_ECG = 250
+FS_EEG = 1000
+FS_GSR = 256
+FS_RESP = 256
+SPLIT_TIME = 45
+STRIDE = 1.0
+EXTENTION_TIME = 2.5
+
+# Features
+FEATURES_N = 1513
+ECG_RAW_N = 11250
+
+# features
+EDA_N = 553
+PPG_N = 11
+Resp_N = 11
+ECG_Resp_N = 13
+ECG_N = 13
+EEG_N = 912
+
+# Path Const
+DATASET_PATH = "G:\\usr\\nishihara\\data\\Yamaha-Experiment\\data\\"
+RESULTS_PATH = "\\results_stride=" + str(STRIDE) + "\\"
+EEG_PATH = RESULTS_PATH + "EEG\\"
+EDA_PATH = RESULTS_PATH + "eda\\"
+PPG_PATH = RESULTS_PATH + "ppg\\"
+RESP_PATH = RESULTS_PATH + "Resp\\"
+ECG_PATH = RESULTS_PATH + "ECG\\"
+ECG_RAW_PATH = RESULTS_PATH + "ECG_RAW\\"
+ECG_RESP_PATH = RESULTS_PATH + "ECG_resp\\"
+TENSORBOARD_PATH = "D:\\usr\\pras\\result\\tensor-board\\valence-arousal\\sMCL\\"
+
+#results raw
+RESULTS_RAW_PATH = "\\results_raw\\"
+EEG_RAW_PATH = RESULTS_RAW_PATH + "EEG\\"
+EDA_RAW_PATH = RESULTS_RAW_PATH + "eda\\"
+PPG_RAW_PATH = RESULTS_RAW_PATH + "ppg\\"
+RESP_RAW_PATH = RESULTS_RAW_PATH + "Resp\\"
+ECG_RAW_PATH = RESULTS_RAW_PATH + "ECG\\"
+ECG_RAW_RESP_PATH = RESULTS_RAW_PATH + "ECG_resp\\"
+
+
+#manager
+CHECK_POINT_PATH = "D:\\usr\\pras\\result\\ValenceArousal\\sMCL\\"
