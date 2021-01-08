0cc2ccfc62eddfbd0743355be3cd61a1a5af9bba
westwing84 <nishihara-tsubasa-gn@ynu.jp>
2021-01-08 22:50:00 +0900

    Add data augmentation layers

--- ContrastiveLearning/Benchmark/BenchModel.py
+++ ContrastiveLearning/Benchmark/BenchModel.py
@@ -1,16 +1,19 @@
 import tensorflow as tf
 from tensorflow.keras.layers import Dense, Conv2D, BatchNormalization, Dropout, ReLU
 from tensorflow.keras.applications.resnet50 import ResNet50
+from tensorflow.keras.layers.experimental.preprocessing import RandomCrop, RandomContrast
 
 
-def createCLModel(input_tensor, num_output):
-    resnet50 = ResNet50(include_top=False, weights=None, input_tensor=input_tensor, pooling="avg")
-    x = resnet50.output
+def createCLModel(input_tensor):
+    x = RandomCrop(25, 25)(input_tensor)
+    augmented = RandomContrast(0.2)(x)
+    resnet50 = ResNet50(include_top=False, weights=None, input_tensor=augmented, pooling="avg")
+    h = resnet50.output
+    x = h
     for units in [128, 128]:
         x = Dense(units=units)(x)
         x = BatchNormalization()(x)
         x = ReLU()(x)
-
-    logits = Dense(units=num_output)(x)
-    return logits
+    z = x
+    return h, z
 
--- ContrastiveLearning/Benchmark/PretrainEncoder.py
+++ ContrastiveLearning/Benchmark/PretrainEncoder.py
@@ -2,8 +2,6 @@ from tensorflow.keras.layers import Input
 from tensorflow.keras.models import Model
 from tensorflow.keras.utils import plot_model, to_categorical
 from tensorflow.keras.datasets import cifar10
-from tensorflow.keras.preprocessing import image
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import matplotlib.pyplot as plt
 from ContrastiveLearning.Benchmark.BenchModel import createCLModel
 
@@ -16,10 +14,13 @@ x_train = x_train.astype("float32") / 255.0
 x_test = x_test.astype("float32") / 255.0
 y_train = to_categorical(y_train, NUM_CLASSES)
 y_test = to_categorical(y_test, NUM_CLASSES)
+input_shape = x_train.shape[1:4]
+print(input_shape)
 
-input_tensor = Input(shape=(32, 32, 3))
-logits = createCLModel(input_tensor, num_output=4)
-model = Model(input_tensor, logits)
-model.summary()
-plot_model(model, to_file="BenchModel.png", show_shapes=True)
+input_tensor = Input(shape=input_shape)
+h, z = createCLModel(input_tensor)
+encoder = Model(input_tensor, h)
+CL_model = Model(encoder.input, z)
+CL_model.summary()
+plot_model(CL_model, to_file="BenchModel.png", show_shapes=True)
 
